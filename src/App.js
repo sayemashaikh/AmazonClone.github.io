c44a944fa387186ec1bae9dff66cfb249d248eaf
sayemashaikh <68963471+sayemashaikh@users.noreply.github.com>
2021-10-09 19:09:37 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Home from './Home';
-import {BrowserRouter as Router,Switch, Route}from"react-router-dom"
+import Header from './Header.js';
+import Home from './Home.js';
+import Checkout from './Checkout.js'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <Checkout/>
-          </Route>
-          <Route path="/">
-            <Header />
-           
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+return (
+<Router>
+<div className="App">
+<Switch>
+<Route path="/checkout">
+<Header />
+<Checkout />
+</Route>
+
+<Route path="/">
+<Header />
+<Home />
+</Route>
+
+</Switch>
+</div>
+
+</Router>
+);
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from "@mui/material";
+
 
 function Header() {
     return (
         <div className="header">
+          <Link to="/">
           <img className="header--logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon"/>
-
+        </Link>
+        
         <div className="header--search">
           <input className= "header--searchInput" type ="text"/>
           {/*Material UI*/}
@@ -30,11 +34,14 @@ function Header() {
             <span className="header--optionLineTwo">Prime</span>
             </div>
         </div>
+        <Link to="/checkout">
         <div className="header--optionBasket">
         <ShoppingCartIcon />
         <span className="header--optionLineTwo header--basketCount">0</span>
         </div>
+        </Link>
         </div>
+      
 
         
     )
